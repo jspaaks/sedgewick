0bf880fdb56884b28871c5eceed05dd7b8776262
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-31 08:42:31 -0500

    added solution to exercise 3.11

---
+++ src/108903011/main.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void) {
+    const size_t n = 99;
+    uint8_t arr[99] = {};
+    for (size_t i = 0; i < n; i++) {
+        arr[i] = 98 - i;
+    }
+    for (size_t i = 0; i < n; i++) {
+        arr[i] = arr[arr[i]];
+    }
+    for (size_t i = 0; i < n; i++) {
+        if (i > 0 && i % 10 == 0) {
+            fprintf(stdout, "\n");
+        }
+        fprintf(stdout, " %2hhu", arr[i]);
+    }
+    fprintf(stdout, "\n");
+    exit(EXIT_SUCCESS);
+}
