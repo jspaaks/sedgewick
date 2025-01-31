49a09b1f986d6f173014867c4b33d4fd302874e9
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-31 11:14:30 -0500

    added comments and memory deallocation

--- src/108700001/main.c
+++ src/108700001/main.c
@@ -14,7 +14,11 @@ bool heads (void) {
 
 
 int main(int argc, const char * argv[]) {
+
+    // initialize the pseudo-random number generator
     srand(time(nullptr));
+
+    // collect user input
     const size_t npositionals = 2;
     const Kwargs * kwargs = kwargs_create(argc, argv, 0, nullptr, npositionals);
     if (kwargs_requires_help(kwargs)) {
@@ -24,10 +28,14 @@ int main(int argc, const char * argv[]) {
     }
     size_t ntosses = options_get_ntosses(kwargs);
     size_t ntrials = options_get_ntrials(kwargs);
+
+    // allocate memory and set initial value
     size_t * counts = malloc((ntosses + 1) * sizeof(size_t));
     for (size_t itoss = 0; itoss <= ntosses; itoss++) {
         counts[itoss] = 0;
     }
+
+    // do the experiment
     for (size_t itrial = 0; itrial < ntrials; itrial++) {
         size_t cnt = 0;
         for (size_t itoss = 0; itoss <= ntosses; itoss++) {
@@ -37,6 +45,8 @@ int main(int argc, const char * argv[]) {
         }
         counts[cnt]++;
     }
+
+    // reporting
     for (size_t itoss = 0; itoss <= ntosses; itoss++) {
         printf("%2zu ", itoss);
         for (size_t i = 0; i < counts[itoss]; i += 10) {
@@ -44,4 +54,9 @@ int main(int argc, const char * argv[]) {
         }
         printf("\n");
     }
+
+    // deallocate memory and exit
+    free(counts);
+    counts = nullptr;
+    exit(EXIT_SUCCESS);
 }
