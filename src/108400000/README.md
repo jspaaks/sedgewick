179127fc84eb110155d94a7d325e493d8ca58cc8
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-02-03 12:10:22 -0500

    fixed indent; added return value; added note about possible integer overflow

---
+++ src/108400000/README.md
@@ -0,0 +1,8 @@
+# 108400000
+
+Reference program 3.5 -- Sieve of Eratosthenes
+
+```console
+$ ./108400000 
+   2    3    5    7   11   <output omitted>   9941 9949 9967 9973 
+```
--- src/108400000/main.c
+++ src/108400000/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+
+// note: values of N larger than sqrt(INT_MAX) won't work or won't
+// work correctly due to integer overflow of term i*j. On systems
+// with 32 bit signed integers, that means 46340 is the maximum
+// allowed value
 #define N 10000
 
+
 int main (void)
-  {  int i, j, a[N];
+  { int i, j, a[N];
     for (i = 2; i < N; i++) a[i] = 1;
     for (i = 2; i < N; i++)
       if (a[i])
-         for (j = i; i*j < N; j++) a[i*j] = 0;
+        for (j = i; i*j < N; j++) a[i*j] = 0;
     for (i = 2; i < N; i++)
       if (a[i]) printf("%4d ", i);
     printf("\n");
+    return 0;
   }
